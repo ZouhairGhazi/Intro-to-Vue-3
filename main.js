850aef4e0575f2bca45ccf204a7651281ddc39ef
Zouhair <zouhair.ghazi1999@gmail.com>
2022-02-14 13:26:37 +0100

    Added solution

--- main.js
+++ main.js
@@ -11,5 +11,17 @@ const app = Vue.createApp({
               { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
             ]
         }
+    },
+    methods: {
+        addToCart() {
+            this.cart++;
+        },
+        removeFromCart() {
+            if (this.cart > 0)
+                this.cart--;
+        },
+        updateImage(image) {
+            this.image = image
+        }
     }
 })
